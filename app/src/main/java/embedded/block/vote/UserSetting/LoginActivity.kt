75f87ccff6d3724f99813cc0f82c3f1d40c0b397
Younjae <jhsl31@naver.com>
2019-06-05 14:31:17 +0900

    액티비티 정리

--- app/src/main/java/embedded/block/vote/UserSetting/LoginActivity.kt
+++ app/src/main/java/embedded/block/vote/UserSetting/LoginActivity.kt
@@ -34,38 +34,40 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        //기본 ipAdress 주소를 사설 IP주소로 할당
         ipAdress = "http://192.9.44.53:"
 
-
+        //회원가입 액티비티 호출 버튼
         regBtn.setOnClickListener {
             var intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
-
+        //로딩 화면 함수 호출, 로그인 함수 호출
         loginBtn.setOnClickListener {
             progressON(this, "로그인중")
             login(myID.text.toString(), myPass.text.toString())
         }
-
+        //아이디 찾기 액티비티 호출 버튼
         findID.setOnClickListener {
             var intent = Intent(this, FindidActivity::class.java)
             startActivity(intent)
         }
-
+        //비밀번호 찾기 액티비티 호출 버튼
         findPass.setOnClickListener {
             var intent = Intent(this, FindpassActivity::class.java)
             startActivity(intent)
         }
     }
-
+    //로그인 함수
     fun login(myid: String, mypass: String) {
         var json = JSONObject()
         json.put("myid", "null!")
-
+        //Volley를 사용하며 REST API를 활요한 서버 통신
         var queue: RequestQueue = Volley.newRequestQueue(this);
         val request = object : JsonObjectRequest(Request.Method.GET, ipAdress + "65001/bote/login/?myid=" + myid + "&mypass=" + mypass, json,
             Response.Listener { response ->
                 run {
+                    //로그인 성공시 회원 데이터를 받아와서 Companion Object에 선언된 변수들에 저장
                     progressOFF()
                     retryCount = 0
                     userClass.clear()
@@ -83,24 +85,28 @@ class LoginActivity : AppCompatActivity() {
                         userNum = response.getString("usernum")
                         userPhone = response.getString("userphone")
                         userAuthor = response.getString("userauthor")
-
+                        //회원 권한이 투표 등록자일 경우 해당 액티비티 호출
                         if(userAuthor == "1") {
                             var intent = Intent(this, AdminActivity::class.java)
                             startActivity(intent)
                         }
+                        //회원 권한이 유권자일 경우 해당 액티비티 호출
                         else if(userAuthor == "2") {
                             var intent = Intent(this, VoteListActivity::class.java)
                             startActivity(intent)
                         }
+                        //회원 권한이 탈퇴한 유저일 경우 Toast 메시지 띄우기
                         else if(userAuthor == "3") {
                             Toast.makeText(this, "회원 탈퇴한 정보입니다", Toast.LENGTH_SHORT).show()
                         }
                     }
+                    //로그인  실패시 Toast 메시지 띄우기
                     else {
                         Toast.makeText(this, "아이디 혹은 비밀번호가 틀렸습니다", Toast.LENGTH_SHORT).show()
                     }
                 }
             }, Response.ErrorListener {
+                //서버가 중단될 경우 3번까지 재시도하면서 사설IP와 공인IP를 전환해가며 접속 시도
                 retryCount++
                 if(retryCount <= 2) {
                     if(ipAdress == "http://203.249.127.32:")
@@ -109,7 +115,7 @@ class LoginActivity : AppCompatActivity() {
                         ipAdress = "http://203.249.127.32:"
                     login(myid, mypass)
                 }
-
+                //사설IP와 공인IP 모두 접속에 실패한 경우
                 else {
                     progressOFF()
                     Toast.makeText(this, "서버가 작동중이 아닙니다.", Toast.LENGTH_SHORT).show()
@@ -126,7 +132,7 @@ class LoginActivity : AppCompatActivity() {
         }
         queue.add(request)
     }
-
+    //로딩을 화면 구성하여 호출하는 함수
     fun progressON(activity: Activity?, message: String) {
 
         if (activity == null || activity.isFinishing) {
@@ -162,12 +168,13 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
-
+    //로딩화면을 종료하는 함수
     fun progressOFF() {
         if (progressDialog != null && progressDialog!!.isShowing) {
             progressDialog!!.dismiss()
         }
     }
+    //로딩 화면에 메시지를 세팅하는 함수
     fun progressSET(message: String) {
 
         if (progressDialog == null || !progressDialog!!.isShowing) {
@@ -182,7 +189,7 @@ class LoginActivity : AppCompatActivity() {
         }
 
     }
-    //user 정보 저장합니다
+    //로그인시 저장되는 사용자 정보 및 로딩화면, 재시도 횟수를 저장하는 Companion Object
     companion object {
         var userId = ""
         var userPass = ""
